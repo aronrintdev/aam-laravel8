a82ad0dc77822b1d875d6874922963f40f787d34
aronrint <aronrint@gmail.com>
2019-04-07 11:04:58 -0400

    remove old swg comments

--- app/Http/Controllers/AppBaseController.php
+++ app/Http/Controllers/AppBaseController.php
@@ -6,13 +6,6 @@ use InfyOm\Generator\Utils\ResponseUtil;
 use Response;
 
 /**
- * @SWG\Swagger(
- *   basePath="/api/v1",
- *   @SWG\Info(
- *     title="Laravel Generator APIs",
- *     version="1.0.0",
- *   )
- * )
  * This class should be parent class for other API controllers
  * Class AppBaseController
  */
