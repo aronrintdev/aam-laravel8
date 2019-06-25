a15e6bf31e154eb95dac9682819c3e4131052a48
aronrint <aronrint@gmail.com>
2019-06-25 14:20:23 -0400

    fix video path on sample swings

--- database/seeds/TestSwingSeeder.php
+++ database/seeds/TestSwingSeeder.php
@@ -17,13 +17,17 @@ class TestSwingSeeder extends Seeder
         $faker->seed(4321);
 
         for ($z = 0; $z < 20; $z++) {
+            $videoPath = $faker->randomElement([
+                'https://v1sports.com/SwingStore/1811131251493KRU518100.mp4',
+                'https://dl5.webmfiles.org/big-buck-bunny_trailer.webm',
+            ]);
             $id = DB::table('Swings')->insertGetId([
                 'AccountID'          => 2,
                 'SwingStatusID'      => 0,
                 'DateUploaded'       => $faker->dateTimeBetween($startDate = '-10 years', $endDate = 'now', $timezone = 'America/New_York'),
                 'Description'        => $faker->realText($maxNbChars=240),
                 'SportID'            => 'GOLF',
-                'VideoPath'          => $faker->randomNumber().'.mp4',
+                'VideoPath'          => $videoPath,
             ]);
         }
     }
