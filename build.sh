071162c9fd34153bba4f614f11f344557899312a
aronrint <aronrint@gmail.com>
2019-07-24 18:03:56 -0400

    devops: build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+if [ $# -eq 0 ]
+then
+    ENV='test'
+else
+    ENV=$1
+fi
+
+echo 'building for env' $ENV '...'
+
+echo 'decrypting env.'$ENV '...'
+if [ -z $KEY ];then
+	echo 'no env decryption key.  aborting.'
+	exit 1
+fi;
+
+SOURCE=../ docker-compose -f ci/compose-build.yml run -T -w "/app" -e KEY=$KEY php-composer php ci/dot-vault.php decrypt ci/envs/env.$ENV > .env
+
+echo 'publish vendor resources ...'
+SOURCE=../ docker-compose -f ci/compose-build.yml run --rm -w "/app" php-composer php artisan vendor:publish --tag=datatables-buttons --force
+
+echo 'composer dependencies (no dev) ...'
+SOURCE=../ docker-compose -f ci/compose-build.yml run php-composer composer --no-ansi install --no-dev --no-progress --ignore-platform-reqs
+#SOURCE=../ docker-compose -f ci/compose-build.yml run node-yarn   sh -c "cd /app && yarn install && yarn $ENV"
