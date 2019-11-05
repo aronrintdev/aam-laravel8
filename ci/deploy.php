e5c3cb070b764bd8a38aad569ff06da7ac7987b7
aronrint <aronrint@gmail.com>
2019-11-05 11:42:58 -0500

    devops: fix deploy notifications to slack

--- ci/deploy.php
+++ ci/deploy.php
@@ -156,14 +156,14 @@ if (strlen(getenv('GITLOG'))) {
 }
 
 before('success', 'success:check');
-task('success:check', function() {
+task('success:check', function() use($gitlog) {
         $stage = Deployer::get()->getInput()->getArgument('stage');
         if ($stage != 'test') {
             set('slack_success_text', "Deploy to *{{target}}* successful".$gitlog);
             set('slack_webhook', 'https://hooks.slack.com/services/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');
-            after('success', 'slack:notify:success');
         }
 });
+after('success', 'slack:notify:success');
 
 task('deploy', [
     'build',
