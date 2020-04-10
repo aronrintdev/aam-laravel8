f55e1463111475e8f54cae69f7ab44e236ffba6c
aronrint <aronrint@gmail.com>
2020-04-10 08:27:27 -0400

    ui: fix processing loader

--- resources/views/layouts/datatables_css.blade.php
+++ resources/views/layouts/datatables_css.blade.php
@@ -2,3 +2,17 @@
 <link href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.2.2/css/buttons.bootstrap.min.css">
+<style>
+div.dataTables_wrapper div.dataTables_processing {
+    z-index: 10;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    margin-left: 0;
+    margin-top: 0;
+    text-align: center;
+    padding: 1em 0;
+}
+</style>
