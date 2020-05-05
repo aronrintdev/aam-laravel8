7943e3fad0812209a88a55cff105fc4e3d999f39
aronrint <aronrint@gmail.com>
2020-05-05 18:36:33 -0400

    report: fix bubble chart positions

--- resources/views/charts/lesson-stats.blade.php
+++ resources/views/charts/lesson-stats.blade.php
@@ -11,7 +11,7 @@ var myChart = new Chart(ctx, {
     type: 'bubble',
     data: {
         datasets: [{
-            label: 'Lessons',
+            label: 'Ready',
             data: [
             ],
             backgroundColor:
@@ -19,7 +19,38 @@ var myChart = new Chart(ctx, {
             borderColor: 
                 'rgba(255, 99, 132, 1)',
             borderWidth: 1
-        }]
+        },
+        {
+            label: 'Unassigned',
+            data: [
+            ],
+            backgroundColor:
+                'rgba(255, 206, 86, 0.2)',
+            borderColor: 
+                'rgba(255, 206, 86, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'InProgress',
+            data: [
+            ],
+            backgroundColor:
+                'rgba(54, 162, 235, 0.2)',
+            borderColor: 
+                'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Complete',
+            data: [
+            ],
+            backgroundColor:
+                'rgba(153, 102, 255, 0.2)',
+            borderColor: 
+                'rgba(153, 102, 255, 1)',
+            borderWidth: 1
+        },
+        ],
     },
     options: {
         scales: {
@@ -81,7 +112,7 @@ $.get('/lesson-stats').then((response) => {
         if (item.status == 'completed') {
             thisy = 4;
         }
-        myChart.data.datasets[0].data.push({
+        myChart.data.datasets[thisy-1].data.push({
             x:item.academy_code, y: thisy, r:item.cnt*5
         });
     });
