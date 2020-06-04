51492c0c8ee5148da56bf013de574602fb592c5d
aronrint <aronrint@gmail.com>
2020-06-04 13:07:48 -0400

    test: refresh test, double TTL for local env check

--- tests/Feature/Session/SessionApiTest.php
+++ tests/Feature/Session/SessionApiTest.php
@@ -134,7 +134,7 @@ class SessionApiTest extends TestCase
             );
         $this->response->assertStatus(200);
         $this->response->assertJsonStructure(['access_token', 'token_type', 'expires_in']);
-        $this->assertEquals(7200, $this->response->getData()->expires_in);
+        $this->assertEquals(14400, $this->response->getData()->expires_in);
     }
 
 
@@ -166,5 +166,58 @@ class SessionApiTest extends TestCase
             );
         $this->response->assertStatus(401);
         $this->response->assertJsonStructure(['errors']);
+    }
+
+    /**
+     * @test
+     */
+    public function test_refresh_session_sends_new_refresh_token()
+    {
+        $this->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
+        $this->withoutMiddleware(\App\Http\Middleware\EnableCors::class);
+
+        $academyId = 'SHYG';
+        $sam = AccountUser::find(5);
+        //can't do json call with cookies in 5.8
+        $headers = [
+            //'CONTENT_LENGTH' => mb_strlen('', '8bit'),
+            'CONTENT_TYPE' => 'application/json',
+            'Accept' => 'application/json',
+        ];
+        $token   = \JWTAuth::fromUser($sam);
+        $refresh = \bin2hex(\random_bytes(40));
+
+        $headers['Authorization'] = 'Bearer ' . $token;
+
+        $result = \DB::table('jwt_refresh_tokens')->insert([
+            'refresh_token' => $refresh,
+            'user_agent'    => "User-Agent",
+            'user_id'       => $sam->AccountID,
+            'expires'       => \Carbon\Carbon::now()->addMonths(6),
+            'revoked'       => 0,
+        ]);
+        $headers = $this->transformHeadersToServerVars($headers);
+
+        $this->response = $this->actingAs($sam, 'api')
+            ->call('POST', '/api201902/session/refresh',
+                //params
+                [
+                ],
+                //cookies
+                [
+                ],
+                //files
+                [],
+                //server-vars
+                $headers,
+                //body
+                json_encode(['refresh_token' => $refresh ]),
+            );
+        $this->response->assertStatus(200);
+        $this->response->assertJsonStructure(['access_token', 'token_type', 'expires_in', 'refresh_token']);
+        $blob = explode('.', $this->response->getData()->access_token);
+        $jwt = json_decode( base64_decode($blob[1]), true);
+        $this->assertEquals($sam->AccountID, $jwt['aid']);
+        $this->assertEquals($sam->Email, $jwt['sub']);
     }
 }
